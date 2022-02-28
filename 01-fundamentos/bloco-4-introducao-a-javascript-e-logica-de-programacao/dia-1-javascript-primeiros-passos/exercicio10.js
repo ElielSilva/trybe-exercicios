34d816acc0baffcddfa0f21a22fcea934bcb4a8d
eliel <silvaeliel940@gmail.com>
2022-02-28 12:18:04 -0300

    pronto

---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
@@ -0,0 +1,11 @@
+let valor1 = 10*1.2;
+let valor2 = 20;
+let imposto = valor1*0.2;
+
+
+if (valor1 < 0 || valor2 < 0) {
+  console.log('erro');
+} else {
+  const valorCustoTotal = valor1
+  let lucro = valor2 - valor2;
+}
---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio11.js
@@ -0,0 +1,20 @@
+var salario = 1600.50
+
+if (salario <= 1556.94) {
+  salario*0.92;
+} else if (salario>=1556.95 && salario<=2594.92) {
+  salario*0.91;
+} else if (salario>=2594.93 && salario<=5189.82) {
+  salario*0.89;
+} else if (salario>=5.189,82) {
+  salario-570,77;
+}
+if (salario>=1903.99 && salario<=2826.65) {
+  salario*0.925
+} else if (salario>=2826.66 && salario<=3751.05) {
+  salario*0.85
+} else if (salario>=3751.05 && salario<=4664.68) {
+  salario*0.775
+} else if (salario>4664.68) {
+  salario*0.725
+}
