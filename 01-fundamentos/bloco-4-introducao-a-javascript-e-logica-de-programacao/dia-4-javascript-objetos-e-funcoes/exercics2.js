bcacdb73b645b76b6f288bcfb8330e9c8beea14e
eliel <silvaeliel940@gmail.com>
2022-02-28 21:34:28 -0300

    foi

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercics2.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercics2.js
@@ -41,9 +41,60 @@ isMin(array3)
 //4
 
 const array4 =  ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-contado 
-for (const iterator of object) {
-  for (const x of object) {
+
+function contar (array) {
+  var resposta = "";
+  let contador = 0;
+  let y=0;
+  for (let iterator of array) {
+    
+    for (let x of iterator) {
+      y++
+    }
+    if (y >contador) {
+      contador = y;
+      resposta = iterator
+      y=0
+    } else {
+      y= 0
+    }
+  }
+  return console.log(resposta); 
+}
+contar(array4)
+
+//5 
+let array5 = [2, 3, 2, 5, 8, 2, 3];
+function isReplace(params) {
+  let numero;
+  let retidor =0;
+  let contador = 0;
+  for (const iterator of params) {
+    for (const x of params) {
+      if (iterator == x) {
+        contador ++
+      }
+    }
+    if (contador>retidor) {
+      retidor = contador
+      numero = iterator
+      contador=0
+    }
+  }
+  return console.log(numero);
+}isReplace(array5)
+
+
+//6 
+let valor = 5;
+function sum(valor) {
+  let resultado = 0;
+  for (let index = 0; index <= valor; index++) {
+    resultado = resultado+index
     
   }
+  return console.log(resultado);
 }
+sum(valor)
+
+// 7 
